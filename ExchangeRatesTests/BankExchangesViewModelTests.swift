19f416eacf3bb46ecd652a372688d60b06ad8f78
Jeff Tabios <jefftabios1@gmail.com>
2021-03-21 22:10:02 +0800

    Updated unit tests

--- ExchangeRatesTests/BankExchangesViewModelTests.swift
+++ ExchangeRatesTests/BankExchangesViewModelTests.swift
@@ -23,29 +23,4 @@ class BankExchangesViewModelTests: XCTestCase {
         XCTAssertEqual(sut.bankExchanges.count, 6)
     }
     
-    func test_refreshRates() {
-        let sut = BankExchangesViewModel()
-        let exp = expectation(description: "Set Exchanges")
-        
-        sut.fromId = 2
-        sut.toId = 4
-        sut.bankMap[1] = "Some bank"
-        sut.bankMap[2] = "Some bank"
-        sut.bankMap[3] = "Some bank"
-        sut.bankMap[4] = "Some bank"
-        sut.bankMap[5] = "Some bank"
-        sut.bankMap[6] = "Some bank"
-        
-        sut.currencyCodeMap[1] = "CAD"
-        sut.currencyCodeMap[2] = "EUR"
-        sut.currencyCodeMap[3] = "USD"
-        sut.currencyCodeMap[4] = "UAE Dirham"
-        
-        sut.refreshRates {
-            exp.fulfill()
-        }
-        
-        waitForExpectations(timeout: 3)
-        XCTAssertEqual(sut.bankRates.count, 4)
-    }
 }
